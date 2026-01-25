14211139c06202a5efa963dc2344d81123451dd7
Alex Buckley <alexmeb81@outlook.com>
2026-01-25 15:45:28 -0800

    chore(server): Implemented Java Address class

---
+++ server/hose/src/main/java/edu/cwu/capstone/hose/addresses/Address.java
@@ -0,0 +1,45 @@
+package edu.cwu.capstone.hose.addresses;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "addresses")
+@Getter 
+@Setter
+@NoArgsConstructor 
+@AllArgsConstructor 
+@Builder
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String streetAddress;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private String state;
+
+    @Column(nullable = false)
+    private String postalCode;
+
+    @Column(nullable = false)
+    private String country = "USA";
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
