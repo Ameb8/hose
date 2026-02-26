28435a5a18cda84b5e1aaa0a537029262d14820d
Alex Buckley <alexmeb81@outlook.com>
2026-02-26 10:51:26 -0800

    feat(frontend): Merged Aty_S frontend updateds with compare and chat button functionality into branch

--- frontend/buttons.js
+++ frontend/buttons.js
@@ -10,29 +10,69 @@ import { applyMapFilters, resetMapFilters } from "./map/filters.js";
   const chatBanner = document.getElementById('chatIndicatorBanner');
   const filterBtn = document.getElementById('filterToggleBtn');
   const filterPanel = document.getElementById('filterPanel');
+  const chatInput = document.getElementById("chatInput");
+  const sendChatBtn = document.getElementById("sendChatBtn");
+  const chatMessages = document.querySelector(".chat-messages");
 
+function sendMessage() {
+  const text = chatInput.value.trim();
+  if (!text) return;
+
+  const msg = document.createElement("div");
+  msg.textContent = text;
+  msg.classList.add("user-message"); //add AI somewhere around here
+
+  chatMessages.appendChild(msg);
+  chatInput.value = "";
+  chatMessages.scrollTop = chatMessages.scrollHeight;
+}
+
+sendChatBtn.addEventListener("click", sendMessage);
+
+chatInput.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") sendMessage();
+});
+
+
+  //Chat button
+  const chatButton = document.getElementById("chatButton"); 
+  const chatWindow = document.getElementById("chatWindow");
   // By default, chat is NOT open, filter panel is hidden
   let chatOpen = false;
   let filterVisible = false;
 
   // Function to update chat UI based on state
-  function setChatOpen(open) {
-    chatOpen = open;
-    if (chatOpen) {
-      chatBadge.classList.remove('hidden');
-      chatBanner.classList.remove('hidden');
-      chatBtn.style.background = '#166973';
-      chatBtn.style.borderColor = '#ffd966';
-    } else {
-      chatBadge.classList.add('hidden');
-      chatBanner.classList.add('hidden');
-      chatBtn.style.background = '#1e4a5f';
-      chatBtn.style.borderColor = '#569fb0';
-    }
+function setChatOpen(open) {
+  if (open && filterVisible) { 
+    setFilterVisible(false); 
   }
+
+  chatOpen = open;
+
+  if (chatOpen) {
+    chatBadge.classList.remove('hidden');
+    chatWindow.classList.remove('hidden');
+
+    chatBtn.style.background = '#25344F';
+    chatBtn.style.borderColor = '#ffffff';
+
+  } else {
+    chatBadge.classList.add('hidden');
+    chatWindow.classList.add('hidden');
+
+    chatBtn.style.background = '#25344F';
+    chatBtn.style.borderColor = '#ffffff';
+  }
+}
+
 
   // toggle filter panel visibility
   function setFilterVisible(visible) {
+    // Only close chat when OPENING filter
+    if (visible && chatOpen) { 
+      setChatOpen(false); 
+    }
+
     filterVisible = visible;
     if (filterVisible) {
       filterPanel.classList.add('show');
