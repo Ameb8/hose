2e0e8ad8711666440d799a6f143acbc70cae756c
Alex Buckley <alexmeb81@outlook.com>
2026-03-11 00:01:32 -0700

    chore(database): Added flyway migration for DB walk routing fields

---
+++ database/migrations/V18_add_walk_distance_fields.sql
@@ -0,0 +1,4 @@
+ALTER TABLE properties ADD COLUMN bus_stop_distance NUMERIC(4, 2);
+ALTER TABLE properties ADD COLUMN bus_stop_mins INTEGER;
+ALTER TABLE properties ADD COLUMN cwu_distance NUMERIC(4, 2);
+ALTER TABLE properties ADD COLUMN cwu_mins INTEGER;
